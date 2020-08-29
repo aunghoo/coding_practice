b4f36cf5e39f14f58f135d58e258d6a9a5650e44
aunghoo <aung@umich.edu>
2020-08-29 00:20:37 -0400

    Add changes to longest palindrome problem

--- DP/longestPalindrome.py
+++ DP/longestPalindrome.py
@@ -2,48 +2,95 @@
 https://leetcode.com/problems/longest-palindromic-substring
 '''
 
-class Solution(object):
-    def longestPalindromeByBottomUpApproach(self, s):
-        return botUpPalindrome(s)
-    def longestPalindromeByTopDownApproach(self, s):
-        cache = [[0] * len(s) for i in range(len(s))]
+class Solution:
+    '''
+    Test cases:
+    ""
+    "a"
+    "abcdefg"
+    "abcba"
+    "abba"
+    "aibcb"
+    "aaaaaaaaaaaaaaaaaaaaaaaaacaaaaaaaaaaaaaaaaaaa"
+    '''
 
-def botUpPalindrome(s):
-    cache = [[0] * len(s) for i in range(len(s))]
-    # vary by length of the substring being checked
-    for k in range(0, len(s)):
-        for i in range(0, len(s) - k):
-            j = i + k
-            if i == j:
-                cache[i][j] = 1
-            elif i+1 == j:
-                cache[i][j] = 1 if (s[i] == s[j]) else 2
-            else:
-                if cache[i+1][j-1] == 1 and s[i] == s[j]:
-                    cache[i][j] = 1
-                else:
-                    cache[i][j] = 2
+    def longestPalindrome(self, s: str) -> str:
+        return self.dpPalindrome(s)
+        # return self.memoPalindrome(s) # This is the solution with topdown memoization
 
-    for length in range(len(s), 0, -1):
-        for i in range(0, len(s)):
-            j = i + length - 1
-            if (j <= len(s) - 1 and cache[i][j] == 1):
-                return s[i:j+1]
-    return ''
+    def memoPalindrome(self, s):
+        table = [[-1] * len(s) for i in range(len(s))]
+        # Go through various lengths from largest to smallest
+        for length in range(len(s), 0, -1):
+            l = 0
+            while l+length <= len(s):
+                if self.isPalindrome(s, l, l+length-1, table):
+                    return s[l:l+length]
+                l += 1
+        return ''
 
-def topDownPalindrome(s, i, j, cache):
-    if cache[i][j] != 0:
-        return True if cache[i][j] == 1 else False
-    if i == j:
-        cache[i][j] = 1
-        return True
-    elif i+1 == j:
-        cache[i][j] = 1 if (s[i] == s[j]) else 2
-        return True if cache[i][j] == 1 else False
-    else:
-        if (topDownPalindrome(s, i+1, j-1, cache) and s[i] == s[j]):
-            cache[i][j] = 1
+    def isPalindrome(self, s, l, r, table):
+        if l == r:
+            table[l][r] = True
             return True
-        else:
-            cache[i][j] = 2
-            return False
+        if l+1 == r:
+            table[l][r] = s[l] == s[r]
+            return table[l][r]
+        if table[l][r] != -1:
+            return table[l][r]
+        if s[l] == s[r]:
+            table[l][r] = self.isPalindrome(s, l+1, r-1, table)
+            return table[l][r]
+
+    def dpPalindrome(self, s):
+        table = [[-1] * len(s) for i in range(len(s))]
+        longest = ''
+        # Iterate through the table based on lengths
+        # Start with length 1 words, then 2, etc
+        for length in range(len(s)):
+            for l in range(len(s)):
+                r = l + length
+                if r >= len(s):
+                    continue
+                if l == r:
+                    table[l][r] = 1
+                elif l+1 == r:
+                    table[l][r] = 1 if s[l] == s[r] else 0
+                elif s[l] == s[r] and table[l+1][r-1] == 1:
+                    table[l][r] = 1
+                else:
+                    table[l][r] = 0
+                if table[l][r] == 1:
+                    longest = s[l:r+1]
+        return longest
+
+    ''' 
+    Next two functions use Center method
+    
+    '''
+    def getPalindromesFromCenter(self, s):
+        longest = ''
+        for i in range(len(s)):
+            # Get character at i to be center
+            palindrome = self.getLongestPalindromeFromCenter(i, s, False)
+            if len(palindrome) > len(longest):
+                longest = palindrome
+            # Check with both character at i and i + 1 to be center (halfway case)
+            if i != len(s) - 1:
+                palindrome = self.getLongestPalindromeFromCenter(i, s, True)
+                if len(palindrome) > len(longest):
+                    longest = palindrome
+        return longest
+
+    def getLongestPalindromeFromCenter(self, i, s, halfway):
+        l, r = i, i
+        longest = ''
+        if halfway:
+            r = i + 1
+        while l >= 0 and r < len(s):
+            if s[l] != s[r]:
+                return longest
+            longest = s[l:r+1]
+            l -= 1
+            r += 1
+        return longest

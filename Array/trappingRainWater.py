cb0676359c38671fe86ab7a6ee0b34c3428e64ba
aunghoo <aung@umich.edu>
2020-10-05 10:35:42 -0400

    Two pointers efficient solution for Trapping Rain Water problem

--- Array/trappingRainWater.py
+++ Array/trappingRainWater.py
@@ -1,53 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # find the highest elevation\
-        highest = -1
-        highest_i = 0
-        for i, h in enumerate(height):
-            if h > highest:
-                highest = h
-                highest_i = i
-        # mark the absolute highest elevation on the map
-        starting_highest = highest
-        starting_highest_i = highest_i
-
+        # Find trapped waters, going from left to right
         trapped_water = 0
+        current = 0
+        trapped_sofar = 0
+        l, r = 0, len(height)-1
+        max_left, max_right = 0, 0
+        while l < r:
+            if height[l] < height[r]:
+                if height[l] < max_left:
+                    trapped_water += (max_left - height[l])
+                else:
+                    max_left = height[l]
+                l += 1
+            else:
+                if height[r] < max_right:
+                    trapped_water += (max_right - height[r])
+                else:
+                    max_right = height[r]
+                r -= 1
 
-        # go to the left of the highest elevation and keep getting the trapped waters
-        left_highest = 0
-        while(left_highest != -1):
-            left_highest = -1
-            left_highest_i = highest_i
-            # search for the highest on the left of current highest elevation
-            for l in reversed(range(highest_i)):
-                if height[l] > left_highest:
-                    left_highest = height[l]
-                    left_highest_i = l
-            # gather trapped water between the left highest and the highest
-            for i in range(left_highest_i+1, highest_i):
-                trapped_water += (left_highest - height[i])
-
-            highest_i = left_highest_i
-            highest = left_highest
-
-
-        # gather trapped water on the right side
-        highest = starting_highest
-        highest_i = starting_highest_i
-        right_highest = 0
-        while(right_highest != -1):
-            right_highest = -1
-            right_highest_i = highest_i
-            # search for the highest on the right of current highest elevation
-            for r in range(highest_i+1, len(height)):
-                if height[r] > right_highest:
-                    right_highest = height[r]
-                    right_highest_i = r
-            # gather trapped water between the right highest and the current highest
-            for i in range(highest_i+1, right_highest_i):
-                trapped_water += (right_highest - height[i])
-
-            highest_i = right_highest_i
-            highest = right_highest
-            
         return trapped_water
